e4cffdac0502299d035bd576a1103144c4afe02e
skywalker <skywalker418@gmail.com>
2012-08-06 21:00:57 +0800

    add item to list when click

--- js/app.js
+++ js/app.js
@@ -1,7 +1,33 @@
-var App = Em.Application.create();
+var App = Em.Application.create({
+	name: 'TODO'
+});
 
-App.MyView = Em.View.extend({
-  mouseDown: function() {
-    window.alert("hello world!");
+TodosView = Em.View.extend({
+
+  add: function(event){
+  	console.log('clicked');
+  	TodosController.add();
+  },
+  done: function(event){
+  	console.log(event);
+  },
+  todoBinding: 'TodosController.content'
+});
+
+App.todo = Em.Object.create({
+  text: 'eating',
+  isDone: false
+});
+
+TodosController = Em.ArrayController.create({
+  content: [],
+  init: function(){
+        var todo = App.todo;
+        this.pushObject(todo);
+    },
+  add: function(){
+		var todo = App.todo;
+        this.pushObject(todo);
   }
 });
+
