ad711dadc8fe36d9130c13d132d5f42363986c1d
skywalker <suupic@gmail.com>
2012-08-07 03:05:14 +0800

    improve

--- js/app.js
+++ js/app.js
@@ -1,33 +1,44 @@
+// 模型
+Todo = Em.Object.extend({
+  text: null,
+  isDone: false,
+  changeStatus: function(){
+    if (this.isDone == false){
+      this.set('isDone',true)
+    }
+    else{
+      this.set('isDone',false)
+    }
+  }
+});
+
 var App = Em.Application.create({
 	name: 'TODO'
-});
-
-TodosView = Em.View.extend({
-
-  add: function(event){
-  	console.log('clicked');
-  	TodosController.add();
-  },
-  done: function(event){
-  	console.log(event);
-  },
-  todoBinding: 'TodosController.content'
-});
-
-App.todo = Em.Object.create({
-  text: 'eating',
-  isDone: false
 });
 
 TodosController = Em.ArrayController.create({
   content: [],
   init: function(){
-        var todo = App.todo;
-        this.pushObject(todo);
     },
-  add: function(){
-		var todo = App.todo;
-        this.pushObject(todo);
+  add: function(text){
+		var todo = Todo.create({
+      text: text
+    })
+    this.pushObject(todo);
   }
 });
 
+TodosView = Em.View.extend({
+  add: function(event){
+    var myDate = new Date();
+    text = myDate.getMinutes() +':'+ myDate.getSeconds();
+    TodosController.add(text);
+  },
+  done: function(){
+    console.log(event.target);
+    var todo = this.get('context');
+    console.log(this);
+    console.log(this.get( 'content' ));
+  },
+  todoBinding: 'TodosController.content'
+});
